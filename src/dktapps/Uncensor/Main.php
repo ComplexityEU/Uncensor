1c1b018100b1055ac64f2247c7b6285ccdb66e25
Duo Incure <aidenaskwith92@gmail.com>
2022-01-25 17:32:38 +0000

    Make it work again

--- src/dktapps/Uncensor/Main.php
+++ src/dktapps/Uncensor/Main.php
@@ -6,13 +6,17 @@ use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\network\mcpe\protocol\TextPacket;
 use pocketmine\plugin\PluginBase;
+use function file;
+use function implode;
+use function array_map;
+use function preg_replace_callback;
+use function str_replace;
+use function mb_substr;
 
 class Main extends PluginBase implements Listener{
 
-	/** @var string[] */
-	private $words = [];
-	/** @var string */
-	private $regex;
+	private array $words = [];
+	private string $regex;
 
 	public function onEnable() : void{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -31,19 +35,21 @@ class Main extends PluginBase implements Listener{
 
 	private function unfilter(string $message) : string{
 		return preg_replace_callback($this->regex, function($matches){
-			return str_replace($matches[1], mb_substr($matches[1], 0, 1) . "\u{FEFF}" . mb_substr($matches[1], 1), $matches[0]);
+			return str_replace($matches[1], mb_substr($matches[1], 0, 1) . "\x1f" . mb_substr($matches[1], 1), $matches[0]);
 		}, $message);
 	}
 
 	public function onDataPacketSend(DataPacketSendEvent $event) : void{
-		$pk = $event->getPacket();
-		if($pk instanceof TextPacket){
-			if($pk->type !== TextPacket::TYPE_TRANSLATION){
-				$pk->message = $this->unfilter($pk->message);
-			}
-			foreach($pk->parameters as $k => $param){
-				$pk->parameters[$k] = $this->unfilter($pk->parameters[$k]);
-			}
-		}
+        $packets = $event->getPackets();
+        foreach ($packets as $pk) {
+            if ($pk instanceof TextPacket) {
+                if ($pk->type !== TextPacket::TYPE_TRANSLATION) {
+                    $pk->message = $this->unfilter($pk->message);
+                }
+                foreach ($pk->parameters as $k => $param) {
+                    $pk->parameters[$k] = $this->unfilter($pk->parameters[$k]);
+                }
+            }
+        }
 	}
 }
